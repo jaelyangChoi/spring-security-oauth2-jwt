4dd43babc56fb35f2118c3d100b462ff748c051f
Jealyang <jaelyangchoi@gmail.com>
2024-11-16 18:00:31 +0900

    feat: "응답 받은 사용자 정보 DB 저장"

--- src/main/java/org/example/springsecurityoauth2jwt/service/CustomOAuth2UserService.java
+++ src/main/java/org/example/springsecurityoauth2jwt/service/CustomOAuth2UserService.java
@@ -1,6 +1,9 @@
 package org.example.springsecurityoauth2jwt.service;
 
+import lombok.RequiredArgsConstructor;
 import org.example.springsecurityoauth2jwt.dto.*;
+import org.example.springsecurityoauth2jwt.entity.UserEntity;
+import org.example.springsecurityoauth2jwt.repository.UserRepository;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -8,7 +11,10 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -17,7 +23,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         System.out.println("oAuth2User.getAttributes() = " + oAuth2User.getAttributes());
 
         //OAuth2User에 담긴 응답 값이 형식이 서비스마다 상이하기 때문에 응답 인터페이스를 만들어 구현
-        String registrationId = userRequest.getClientRegistration().getClientId();
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
         OAuth2Response oAuth2Response = null;
         if(registrationId.equals("naver")){
             oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
@@ -28,10 +34,31 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
         //리소스 서버에서 발급 받은 정보로 사용자를 특정할 아이디 값을 만듦
         String username = oAuth2Response.getProvider() + " "+oAuth2Response.getProviderId();
+
+
+
+        //DB 저장
+        String role = "ROLE_USER";
+        UserEntity foundUser = userRepository.findByUsername(username);
+        if(foundUser == null){
+            UserEntity userEntity = new UserEntity();
+            userEntity.setUsername(username);
+            userEntity.setName(oAuth2Response.getName());
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setRole(role);
+            userRepository.save(userEntity);
+        }else{
+            role = foundUser.getRole();
+            foundUser.setName(oAuth2Response.getName());
+            foundUser.setEmail(oAuth2Response.getEmail());
+            userRepository.save(foundUser);
+        }
+
+        //응답
         UserDTO userDTO = UserDTO.builder()
                 .username(username)
                 .name(oAuth2Response.getName())
-                .role("ROLE_USER")
+                .role(role)
                 .build();
 
         return new CustomOAuth2User(userDTO);
