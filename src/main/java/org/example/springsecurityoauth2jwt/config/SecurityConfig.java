54086e0d8a203a1bc17399b1bf1144f5b975e118
Jealyang <jaelyangchoi@gmail.com>
2024-11-16 16:44:07 +0900

    feat: "기본적인 SecurityConfig 설정"

---
+++ src/main/java/org/example/springsecurityoauth2jwt/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package org.example.springsecurityoauth2jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(auth -> auth.disable());
+
+        http
+                .formLogin(auth -> auth.disable());
+
+        http
+                .httpBasic(auth -> auth.disable());
+
+        http
+                .oauth2Login(Customizer.withDefaults());
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated());
+
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
