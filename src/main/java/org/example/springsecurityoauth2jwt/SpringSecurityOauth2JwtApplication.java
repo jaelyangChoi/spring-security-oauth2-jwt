4c086792990c4192899e968e607bc851c242d199
Jealyang <jaelyangchoi@gmail.com>
2024-11-16 15:59:24 +0900

    init project

---
+++ src/main/java/org/example/springsecurityoauth2jwt/SpringSecurityOauth2JwtApplication.java
@@ -0,0 +1,13 @@
+package org.example.springsecurityoauth2jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2JwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springsecurityoauth2jwt/SpringSecurityOauth2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springsecurityoauth2jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
